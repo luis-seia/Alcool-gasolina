5ec7aa324a5b57c21a05e2b3509250f45ce11e26
Luis-Seia <luis.seiax@gmail.com>
2023-08-13 16:22:49 +0200

    finish

--- app/src/main/java/com/example/alcoolgasolina/MainActivity.kt
+++ app/src/main/java/com/example/alcoolgasolina/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.example.alcoolgasolina
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.example.alcoolgasolina.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var  binding: ActivityMainBinding;
+    private lateinit var  binding: ActivityMainBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,8 +17,38 @@ class MainActivity : AppCompatActivity() {
         setContentView(view)
 
         binding.button.setOnClickListener{
-            Toast.makeText(this@MainActivity, "clicado", Toast.LENGTH_LONG).show();
+            val alcool =  binding.editPrecoAlcool.text.toString()
+            val  gasolina = binding.editPrecoGasosa.text.toString()
+           val validar = validarCampos(alcool, gasolina)
+            if(validar){
+                calcular(alcool,gasolina)
+            }else{
+                Toast.makeText(this@MainActivity, "Preenca os campos", Toast.LENGTH_LONG).show()
+            }
         }
 
+
+
     }
+
+    @SuppressLint("SetTextI18n")
+    fun calcular(alcool: String, gasolina: String){
+        val precoAlcool = alcool.toDouble()
+        val precogasolina = gasolina.toDouble()
+        val resulatdo = precoAlcool/precogasolina
+        if(resulatdo >= 0.7 ){
+            binding.res.text = "melhor utilizar gasosa"
+        }else{
+            binding.res.text = "melhor utilizar alcool"
+        }
+    }
+
+    fun validarCampos(alcool: String?, gasolina: String?): Boolean {
+        var validar: Boolean = true
+        if (alcool.isNullOrEmpty() || gasolina.isNullOrEmpty()) {
+            validar = false
+        }
+        return validar
+    }
+
 }
