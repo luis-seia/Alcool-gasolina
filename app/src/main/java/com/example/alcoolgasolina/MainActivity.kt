62a13e96b2b79e3ad39c0913991d7d6a23b3a4cb
Luis-Seia <luis.seiax@gmail.com>
2023-08-13 15:32:45 +0200

    test

--- app/src/main/java/com/example/alcoolgasolina/MainActivity.kt
+++ app/src/main/java/com/example/alcoolgasolina/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.alcoolgasolina
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.alcoolgasolina.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var  binding: ActivityMainBinding;
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.button.setOnClickListener{
+            Toast.makeText(this@MainActivity, "clicado", Toast.LENGTH_LONG).show();
+        }
+
     }
 }
